29591fde9b8a2af6cb4f2faef5d44630bf7bcba6
Rbk796 <rebecaaxos@gmail.com>
2020-03-15 20:16:56 +0100

    Mi aplicación Django, segundo commit commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from my_app import views
+from other_app.views import Home
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('', Home.as_view(), name='home'),
+    path('blog/', include('blog.urls'))
 ]
